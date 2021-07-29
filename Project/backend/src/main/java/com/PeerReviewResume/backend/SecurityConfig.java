44e17099b0e66090ef7bec7479680cd063624ab6
ngliuxiang <ngliuxiang@gmail.com>
2021-07-29 01:20:14 -0700

    newest working version
    css working finally

--- Project/backend/src/main/java/com/PeerReviewResume/backend/SecurityConfig.java
+++ Project/backend/src/main/java/com/PeerReviewResume/backend/SecurityConfig.java
@@ -49,8 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure (HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(
-				"/style",
-				"/signUp").permitAll()
+				"/signUp").permitAll().antMatchers("/css/*css").permitAll()
 		.anyRequest()
 		.authenticated()
 		.and()
