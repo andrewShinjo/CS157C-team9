27fa9c446b506a216393c59c482fabcd62ab0faf
ngliuxiang <ngliuxiang@gmail.com>
2021-07-29 03:14:25 -0700

    new working code

--- Project/backend/src/main/java/com/PeerReviewResume/backend/controllers/ReviewsController.java
+++ Project/backend/src/main/java/com/PeerReviewResume/backend/controllers/ReviewsController.java
@@ -1,14 +1,21 @@
 package com.PeerReviewResume.backend.controllers;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import com.PeerReviewResume.backend.entity.Resume;
+import com.PeerReviewResume.backend.repositories.ResumeRepository;
+import com.PeerReviewResume.backend.repositories.UserCredentialsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.PeerReviewResume.backend.entity.Reviews;
@@ -18,18 +25,32 @@ import com.PeerReviewResume.backend.repositories.ReviewsRepository;
 public class ReviewsController {
 	@Autowired
 	private ReviewsRepository reviewRepo;
-	
+	@Autowired
+	private UserCredentialsRepository userCredentialsRepository;
+	@Autowired
+	private ResumeRepository resumeRepository;
+
+
 	@GetMapping(value = "/review")
 	public String getReviewData(Model model) {
 		model.addAttribute("reviews", new Reviews());
 		return "pages/review";
 	}
 	
-	@PostMapping({"review", "/resume/review_content"})
-	public String onSubmit(@ModelAttribute Reviews reviews, @ModelAttribute Resume resumes) {
-		reviews.setUserid(resumes.getUserid());
+	@PostMapping(value ="review")
+	public String onSubmit(@ModelAttribute Reviews reviews) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String email = auth.getName();
+		UUID currentUserId = userCredentialsRepository.findByEmail(email).get().getUserid();
+		List<Resume> resumes = new ArrayList<>();
+		resumeRepository.findAll().forEach(resumes::add);
+		for (Resume resume: resumes) {
+			if (!resume.getUserid().equals(currentUserId)) {
+				reviews.setUserid(resume.getUserid());
+				reviews.setField(resume.getField());
+			}
+		}
 		reviews.setDate(new Date());
-		reviews.setField(resumes.getField());
 		reviewRepo.save(reviews);
 		return "pages/review";
 	}
